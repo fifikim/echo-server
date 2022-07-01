22b125e6d9559928d559347e3eea09ba18c2cc8e
Sophia Kim <mail@fifikim.com>
2022-07-01 15:01:27 -0400

    update test

--- app/src/test/java/echoserver/AppTest.java
+++ app/src/test/java/echoserver/AppTest.java
@@ -1,16 +1,10 @@
 package echoserver;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 public class AppTest {
-  @Test public void appHasGreeting() {
-    App classUnderTest = new App();
-    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-  }
-
   @Test public void appHasCorrectGreeting() {
     App classUnderTest = new App();
     assertEquals("Hello World!", classUnderTest.getGreeting());
